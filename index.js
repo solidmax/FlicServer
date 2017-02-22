2ed06d3c3718dcfba6713a08a01f4917c407f80b
solidmax <maxi.zamora@gmail.com>
2017-02-21 23:09:39 -0300

    index html corregido

--- index.js
+++ index.js
@@ -1,39 +1,3 @@
-
-/*var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-var port = process.env.PORT || 3000;
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-
-//eventos para la pagina web
-io.on('connection', function(socket){
-    console.log("connection");    
-    io.emit("welcome","Bienvenido al servidor!");
-
-
-    socket.on('NewLatLong', function(latLong){
-        console.log("NewLatLong received: "+latLong);
-        
-        io.emit('NewLatLong', latLong);
-    });
-
-    socket.on('message',function(data){
-        console.log("New message received: "+data);
-    });
-    socket.on('id',function(data){
-        console.log("New message from "+data);
-    });
-});
-
-
-http.listen(port, function(){
-    console.log("escuchando puerto %d",port);
-});
-*/
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -48,8 +12,7 @@ app.use(express.static(__dirname + '/public'));
 //app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  //response.render('pages/index');
-  //response.render(__dirname + '/index.html');
+  //response.render('pages/index');  
    response.sendFile(__dirname + '/index.html');
   
 });
