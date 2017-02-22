652d29f3a6d8e2cffb3039ddbb940e74baa52843
solidmax <maxi.zamora@gmail.com>
2017-02-21 22:46:36 -0300

    servidor usando algunas partes del ejemplo de heroku

--- index.js
+++ index.js
@@ -36,21 +36,51 @@ http.listen(port, function(){
 */
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  //response.render('pages/index');
+  //response.render(__dirname + '/index.html');
+   response.sendFile(__dirname + '/index.html');
+  
 });
 
+//eventos para la pagina web
+io.on('connection', function(socket){
+    console.log("connection");    
+    io.emit("welcome","Bienvenido al servidor!");
+
+
+    socket.on('NewLatLong', function(latLong){
+        console.log("NewLatLong received: "+latLong);
+        
+        io.emit('NewLatLong', latLong);
+    });
+
+    socket.on('message',function(data){
+        console.log("New message received: "+data);
+    });
+    socket.on('id',function(data){
+        console.log("New message from "+data);
+    });
+});
+
+
+http.listen(app.get('port'), function(){
+    console.log("escuchando puerto %d",app.get('port'));
+});
+/*
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+*/
